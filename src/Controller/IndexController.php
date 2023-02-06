2061fd16c148039c5c33090577a615d5caf952f3
Pierre Granger <pierre.granger@apidae-tourisme.com>
2023-02-06 20:35:34 +0100

    init symfony

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace App\Controller ;
+
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\Routing\RouterInterface;
+    use Symfony\Component\Routing\Annotation\Route;
+
+    class IndexController extends AbstractController
+    {
+        #[Route('/', name: 'index')]
+        public function index()
+        {
+            return $this->render('base.html.twig');
+        }
+    }
